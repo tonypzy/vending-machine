d0722f17889c2ef28583e9076ecf30be149fcf1a
peng <peng.1139@osu.edu>
2025-11-11 23:38:24 -0500

    Add a text chat bot, whose code logic is the same as audio bot

--- import google.py
+++ import google.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-genai.configure(api_key="YOUR_API_KEY")
+genai.configure(api_key="AIzaSyCWQaE8mBjc8x93wbXlgK1-pi4yJ3MzbnM")
 
 for m in genai.list_models():
     print(m.name)
